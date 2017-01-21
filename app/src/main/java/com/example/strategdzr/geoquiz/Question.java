677ea1ec139be6b00ab1803a6a99cfc8eb3096b4
Alexey Shein <strategdzr@gmail.com>
2017-01-21 12:01:11 +0300

    update the controller

--- app/src/main/java/com/example/strategdzr/geoquiz/Question.java
+++ app/src/main/java/com/example/strategdzr/geoquiz/Question.java
@@ -7,6 +7,11 @@ public class Question {
     private int mTextResId;
     private boolean mAnswerTrue;
 
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
     public int getTextResId() {
         return mTextResId;
     }
--- app/src/main/java/com/example/strategdzr/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/strategdzr/geoquiz/QuizActivity.java
@@ -2,21 +2,92 @@ package com.example.strategdzr.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private TextView mQuestionTextView;
+    private TextView mQuestionTextView; //create fields
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPrevButton;
-    private Question[] mQuestionBank = new Question[];
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.q_oceans, true),
+            new Question(R.string.q_mideast, false),
+            new Question(R.string.q_africa, false),
+            new Question(R.string.q_americas, true),
+            new Question(R.string.q_asia, true),
+    };
+    private int mCurrentIndex = 0;
+
+    private void checkAnswer(boolean userPressedTrue) { //show toast about correct or incorrect answer
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+    }
+
+    private void update_question() { //display text (in TextView) which find by textResId in array
+        try {
+            int question = mQuestionBank[mCurrentIndex].getTextResId();
+            mQuestionTextView.setText(question);
+        } catch (ArrayIndexOutOfBoundsException e) { //when try tap on PrevButton on first question
+            e.printStackTrace();
+            Toast.makeText(this, "No questions before!",Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button); //init and find objects
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() { //set listeners
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                update_question();
+            }
+        });
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                update_question();
+            }
+        });
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                update_question();
+            }
+        });
+
+        update_question();
     }
 }
